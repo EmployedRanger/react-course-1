e5476ca69bc4a0b3d6a8c4a96054f78c41ac40f8
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-03-07 13:31:04 -0700

    Added more notes

--- code/03 React Essentials/06-alternative-props-syntax/src/App.jsx
+++ code/03 React Essentials/06-alternative-props-syntax/src/App.jsx
@@ -53,6 +53,21 @@ function App() {
       </main>
     </div>
   );
+
+  <div id="app" data-testid="app">
+    <h1>Time to Practice</h1>
+    <p>One course, many goals! 🎯</p>
+    <ul>
+      <CourseGoal
+        title="Learn React"
+        description="In-depth"
+      />
+      <CourseGoal
+        title="Practice"
+        description="Practice all the things"
+      />
+    </ul>
+  </div>
 }
 
 export default App;
