e7593a819f2ee796c0ac2776655af490dd09255a
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-03-21 16:04:09 -0600

    Added editing player names

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import Player from './components/Player.jsx';
 
 function App() {
 
@@ -5,25 +6,13 @@ function App() {
   return <main>
     <div id="game-container">
       <ol id="players">
-        <li>
-          <span>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </span>
-          <button>Edit</button>
-        </li>
-        <li>
-          <span>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </span>
-          <button>Edit</button>
-        </li>
-
+        <Player name="Player 1" symbol="X" />
+        <Player name="Player 2" symbol="O" />
       </ol>
 
       GAME BOARD
     </div>
+
   </main>
 }
 
