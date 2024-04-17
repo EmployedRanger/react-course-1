80f73da8b53c4a9552ec5935e273d8cea53baf02
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-04-17 15:25:14 -0600

    Finished project

--- code/14 Class-based Components/08-finished/src/App.js
+++ code/14 Class-based Components/08-finished/src/App.js
@@ -1,6 +1,7 @@
 import UserFinder from './components/UserFinder';
 import UsersContext from './store/users-context';
 
+
 const DUMMY_USERS = [
   { id: 'u1', name: 'Max' },
   { id: 'u2', name: 'Manuel' },
