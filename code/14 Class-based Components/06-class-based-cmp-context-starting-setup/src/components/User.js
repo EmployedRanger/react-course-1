9eec4714d562a890d178e972b63d82e501dd05b0
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-04-17 15:24:40 -0600

    Added notes and started project

--- code/14 Class-based Components/06-class-based-cmp-context-starting-setup/src/components/User.js
+++ code/14 Class-based Components/06-class-based-cmp-context-starting-setup/src/components/User.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-
 import classes from './User.module.css';
 
 class User extends Component {
@@ -11,9 +10,5 @@ class User extends Component {
     return <li className={classes.user}>{this.props.name}</li>;
   } 
 }
-
-// const User = (props) => {
-//   return <li className={classes.user}>{props.name}</li>;
-// };
 
 export default User;
