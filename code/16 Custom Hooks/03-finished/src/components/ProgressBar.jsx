e5a675b6730bd86ff2994c74a2424970054bd284
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-04-17 15:28:58 -0600

    Followed along with video

--- code/16 Custom Hooks/03-finished/src/components/ProgressBar.jsx
+++ code/16 Custom Hooks/03-finished/src/components/ProgressBar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 
+
 export default function ProgressBar({ timer }) {
   const [remainingTime, setRemainingTime] = useState(timer);
 
--- code/16 Custom Hooks/03-finished/src/hooks/useFetch.js
+++ code/16 Custom Hooks/03-finished/src/hooks/useFetch.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 
+
 export function useFetch(fetchFn, initialValue) {
   const [isFetching, setIsFetching] = useState();
   const [error, setError] = useState();
